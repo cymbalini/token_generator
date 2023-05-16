a7ca423e714104b31296a69291badf2472f23019
Krzysztof Nowak <992haslo992@gmail.com>
2023-05-17 01:32:10 +0200

    input with value

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const text="aavvdef1234"
 const texttab=['a','b','c']
 const generatetonken=(length=8)=>{
-    const string='abcdef*&^%$12345'
+    const string='!"#$%&\()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
     const tabrand=[]
     for(let i =0;i<length;i++){
         const char=(Math.random()*(string.length-1)).toFixed(0)
@@ -12,5 +12,6 @@ const generatetonken=(length=8)=>{
 }
 const btn=document.querySelector('button')
 btn.addEventListener('click',()=>{
-    document.querySelector('div').innerText=generatetonken(40)
+    const liczba=document.querySelector('#ilosc').value
+    document.querySelector('div').innerText=generatetonken(liczba)
 })
